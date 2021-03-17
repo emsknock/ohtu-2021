7b58525f744a0dd917ea5120b6e956dd3a68ea6c
Emma Koskinen <25252659+emsknock@users.noreply.github.com>
2021-03-17 17:05:43 +0200

    Set up stats test

---
+++ Week 1/nhl-statistics-1/src/tests/statistics_test.py
@@ -0,0 +1,20 @@
+import unittest
+from statistics import Statistics
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatistics(unittest.TestCase):
+    def setUp(self):
+        print("Set up goes here")
+
+    def test_hello_world(self):
+        self.assertEqual("Hello world", "Hello world")
