f3abf026c4f634631003ab2e6db3504a237b9179
emsknock <25252659+emsknock@users.noreply.github.com>
2021-04-26 15:40:59 +0300

    Show players

--- Week 3/nhl-reader/src/index.py
+++ Week 3/nhl-reader/src/index.py
@@ -1,8 +1,11 @@
 from playerReader import PlayerReader
+from playerStats import PlayerStats
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players"
     reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    for player in stats.top_scorers_by_nationality("FIN"): print(player)
 
 if __name__ == "__main__":
     main()
