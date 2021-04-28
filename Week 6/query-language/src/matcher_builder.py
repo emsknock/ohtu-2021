0833ae92706b25fa01570b1ae0f8c4eaaf3ee042
emsknock <25252659+emsknock@users.noreply.github.com>
2021-04-28 19:48:24 +0300

    Create matcher builder

---
+++ Week 6/query-language/src/matcher_builder.py
@@ -0,0 +1,29 @@
+from matchers import (
+    All,
+    And,
+    PlaysIn,
+    HasAtLeast,
+    HasFewerThan
+)
+
+class MatcherBuilder:
+    def __init__(self):
+        self._matcher = All()
+
+    def _and(self, matcher):
+        self._matcher = And(self._matcher, matcher)
+
+    def build(self):
+        return self._matcher
+
+    def playsIn(self, team):
+        self._and(PlaysIn(team))
+        return self
+
+    def hasAtLeast(self, value, attr):
+        self._and(HasAtLeast(value, attr))
+        return self
+
+    def hasFewerThan(self, value, attr):
+        self._and(HasFewerThan(value, attr))
+        return self
